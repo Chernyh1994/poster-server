b6325c3578c429e3053b8bc10354018cb5fc8293
Chernyh1994 <chernuh.1994@gmail.com>
2020-03-18 17:48:14 +0200

    comment added laravel/airlock created authorization

--- app/User.php
+++ app/User.php
@@ -5,11 +5,12 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Airlock\HasApiTokens;
 
 class User extends Authenticatable
 {
+    use HasApiTokens, Notifiable;
     use Notifiable;
-
     /**
      * The attributes that are mass assignable.
      *
--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,15 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('airlock')->namespace('API')->group(function() {
+    Route::post('register', 'AuthController@register');
+    Route::post('token', 'AuthController@token');
 });
+
+Route::middleware('auth:airlock')->get('/name', function (Request $request) {
+    return response()->json(['name' => $request->user()->name]);
+});
+
+
+
+
