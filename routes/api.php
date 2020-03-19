16bb9485314ecc5e8a6aeaaff6c6cd12261e7c60
Chernyh1994 <chernuh.1994@gmail.com>
2020-03-19 08:38:14 +0200

    comment update route Api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,13 +19,45 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('airlock')->namespace('Auth')->group(function() {
     Route::post('register', 'AuthController@register');
     Route::post('token', 'AuthController@token');
-    Route::post('login', 'LoginController@login');
 });
 
-Route::middleware('auth:airlock')->get('/name', function (Request $request) {
-    return response()->json(['name' => $request->user()->name]);
+// Route::middleware('auth:airlock')->get('/name', function (Request $request) {
+//     return response()->json(['name' => $request->user()->name]);
+// });
+
+Route::middleware('auth:airlock')->get('/user', function (Request $request) {
+    return $request->user();
 });
 
+Route::middleware('auth:airlock')->post('/logout', function (Request $request) {
+    $request->user()->tokens()->delete();
 
+    return response('Loggedout', 200);
+});
+
+Route::post('/airlock/token', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        'device_name' => 'required'
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (!$user || !Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+
+    $token = $user->createToken($request->device_name)->plainTextToken;
+
+    $response = [
+        'user' => $user,
+        'token' => $token,
+    ];
+
+    return response($response, 201);
+});
 
 
