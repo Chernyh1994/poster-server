4017dc0ca3818052f0185df293dc0ef586558451
Chernyh1994 <chernuh.1994@gmail.com>
2020-04-11 13:23:04 +0300

    comment

--- app/Http/Controllers/V1/CommentController.php
+++ app/Http/Controllers/V1/CommentController.php
@@ -52,7 +52,7 @@ class CommentController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return ResponseJson
      */
     public function update(Request $request, $id)
     {
