e18cb754b124c4fcb82d8e6247834ba352898c44
Chernyh1994 <chernuh.1994@gmail.com>
2020-04-16 19:44:50 +0300

    comment added avatar colum in users tables, update user method in AuthController

--- app/Http/Controllers/V1/AuthController.php
+++ app/Http/Controllers/V1/AuthController.php
@@ -4,9 +4,12 @@ namespace App\Http\Controllers\V1;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Arr;
 use Illuminate\Http\Request;
 use App\Http\Requests\V1\Auth\RegisterRequest;
 use App\Http\Requests\V1\Auth\LoginRequest;
+use App\Http\Requests\V1\Auth\UpdateUserRequest;
 use App\Models\User;
 
 class AuthController extends Controller
@@ -45,16 +48,37 @@ class AuthController extends Controller
         $token = $user->createToken($request->userAgent())->plainTextToken;
         return response()->json(compact('user', 'token'));
     }
-   /**
+    /**
      * Handle an  user.
      *
      * @param Request $request
      *
      * @return ResponseJson
      */
-   public function name(Request $request)
-   {
+    public function name(Request $request)
+    {
         $user = Auth::user();
         return response()->json(compact('user'));
-   }
+    }
+
+    /**
+    * Update user.
+    *
+    * @param UpdateUserRequest $request
+    *
+    * @return ResponseJson
+    */
+    public function update(UpdateUserRequest $request)
+    {
+        $user_id = Auth::id();
+        $user = User::findOrFail($user_id);
+        $data = $request->validated();
+        if($request->file('avatar')){
+            $path = $request->file('avatar')->store('upload/avatars', 'public');
+            $data = Arr::add($data, 'avatar_path', $path);
+        }
+        $user->fill($data)->save();
+        $user = Auth::user();
+        return response()->json(compact('user'));
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ Route::middleware('api')->prefix('V1')->namespace('V1')->group(function() {
     Route::post('register', 'AuthController@register');
     Route::post('login', 'AuthController@login');
     Route::get('user', 'AuthController@name')->middleware('auth:sanctum');
+    Route::put('user', 'AuthController@update')->middleware('auth:sanctum');
 
     Route::middleware('auth:sanctum')->group(function() {
         Route::apiResource('post', 'PostController');
