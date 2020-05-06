812d4daeb4a1ad1dd9a01301e0af0e48daf44c44
Chernyh1994 <chernuh.1994@gmail.com>
2020-05-06 15:30:44 +0300

    comment update route for post controller

--- routes/api.php
+++ routes/api.php
@@ -5,13 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('api')->prefix('V1')->namespace('V1')->group(function() {
     Route::post('register', 'AuthController@register');
     Route::post('login', 'AuthController@login');
+    Route::apiResource('post', 'PostController');
 
     Route::middleware('auth:sanctum')->group(function() {
-        Route::apiResource('post', 'PostController');
-        Route::get('user/posts', 'PostController@showMyPosts');
-
         Route::apiResource('post.comment', 'CommentController');
-        
+        Route::get('user/posts', 'PostController@showMyPosts');
         Route::get('user', 'UserController@myProfile');
         Route::put('user', 'UserController@update');
         Route::get('user/{id}', 'UserController@userProfile');
